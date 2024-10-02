3d851b8116f4f8700214463aa2f075aac313f1ed
MikaelDv <enzomikael@hotmail.com>
2024-10-02 09:50:59 -0300

    Amou daquela vez como se fosse a última

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Menu from "../Menu/Menu";
+
 
 export default function Cabecalho(){
     return(
         <header className="cabecalho">
             <Image src="/img/atronaut-svgrepo-com.svg" alt="Astronauta Logo." width={50} height={50}/>
+            <Menu/>
         </header>
     )
 }
